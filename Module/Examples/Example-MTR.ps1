cd328f85f0638bfb4ff64bbba6081be8b98da43e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-14 20:36:49 +0200

    feat(examples): ✨ enhance MTR example with detailed location tracking
    * Added `-SimpleLocations` parameter to `Start-GlobalpingMtr` for specific location analysis.
    * Included additional `ToMtrHops()` calls for better output visualization.

--- Module/Examples/Example-MTR.ps1
+++ Module/Examples/Example-MTR.ps1
@@ -4,10 +4,12 @@ Start-GlobalpingMtr -Target "evotec.xyz" -Verbose | Format-Table
 
 Start-GlobalpingMtr -Target "evotec.xyz" -Verbose -Classic | Format-Table
 
-$OutputMtr = Start-GlobalpingMtr -Target "evotec.xyz" -Verbose -Raw
+$OutputMtr = Start-GlobalpingMtr -Target "evotec.xyz" -Verbose -Raw -SimpleLocations "Krakow+PL","Warsaw+PL", "Berlin+DE", "London+UK", "NewYork+US", "SanFrancisco+US", "Tokyo+JP"
 $OutputMtr | Format-Table
 $OutputMtr.Results | Format-Table
 $OutputMtr.Results.Probe | Format-Table
 $OutputMtr.Results.Data | Format-List
 $OutputMtr.Results[0].Data.RawOutput
-$OutputMtr.Results[0].ToMtrHops() | Format-Table
+$OutputMtr.Results[0].ToMtrHops() | Format-Table *
+$OutputMtr.Results[1].ToMtrHops() | Format-Table *
+$OutputMtr.Results[2].ToMtrHops() | Format-Table *
