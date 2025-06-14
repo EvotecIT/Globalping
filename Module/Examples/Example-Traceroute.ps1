606b10e84a9b9c93fd9aa56c39bc900bc4c2e337
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-14 20:08:47 +0200

    feat(examples): ✨ add DNS, HTTP, MTR, and Traceroute examples
    * Added example scripts for DNS, HTTP, MTR, and Traceroute functionalities.
    * Each script demonstrates the use of `Start-Globalping` commands with verbose output.
    * Enhanced output formatting for better readability.

--- Module/Examples/Example-Traceroute.ps1
+++ Module/Examples/Example-Traceroute.ps1
@@ -1,3 +1,12 @@
 ﻿Import-Module $PSScriptRoot\..\Globalping.psd1 -Force
 
-Start-GlobalpingTraceroute -Target "evotec.xyz" -Verbose | Format-Table
+Start-GlobalpingTraceroute -Target "evotec.xyz" -Verbose | Format-Table *
+
+Start-GlobalpingTraceroute -Target "evotec.xyz" -Verbose -Classic | Format-Table
+
+$OutputTrace = Start-GlobalpingTraceroute -Target "evotec.xyz" -Verbose -Raw
+$OutputTrace | Format-Table
+$OutputTrace.Results | Format-Table
+$OutputTrace.Results.Probe | Format-Table
+$OutputTrace.Results.Data | Format-List
+$OutputTrace
