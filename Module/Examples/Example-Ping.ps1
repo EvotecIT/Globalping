d0e39665bf9bb5a626ab1b45e76b213abdf7ad3b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-14 14:50:44 +0200

    feat: ✨ Add Globalping module and example usage
    * Introduced the `Globalping` module with core functionality to ping hosts globally.
    * Created `Build-Module.ps1` for module configuration and manifest generation.
    * Added example script `Example-Ping.ps1` demonstrating usage of `Start-Globalping`.
    * Defined module metadata in `Globalping.psd1` including author, version, and project URI.

---
+++ Module/Examples/Example-Ping.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module $PSScriptRoot\..\Globalping.psd1 -Force
+
+
+Start-Globalping -Type Ping -Target "evotec.xyz" -Verbose
+
+Start-Globalping -Type Ping -Target "evotec.xyz" -Verbose -Locations "DE"
