39d55e9be6934458eab6c234e9ad6532331125ac
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-14 15:27:09 +0200

    Improve documentation and examples

--- Module/Examples/Example-Ping.ps1
+++ Module/Examples/Example-Ping.ps1
@@ -1,4 +1,4 @@
-﻿Import-Module $PSScriptRoot\..\Globalping.psd1 -Force
+Import-Module $PSScriptRoot\..\Globalping.psd1 -Force
 
 Start-Globalping -Type Ping -Target "evotec.xyz" -Verbose | Format-Table
 
@@ -6,4 +6,17 @@ Start-Globalping -Type Ping -Target "evotec.xyz" -Verbose -SimpleLocations "DE" 
 
 Start-Globalping -Type Ping -Target "evotec.xyz" -Verbose -SimpleLocations "DE", "US", "UK" | Format-Table
 
-Start-Globalping -Type Ping -Target "evotec.xyz" -Verbose -Locations "DE", "US", "UK", "PL", "FR", "IT", "ES", "NL", "SE", "CH" | Format-Table
+$loc = @(
+    [Globalping.LocationRequest]@{ Country = "DE"; Limit = 1 },
+    [Globalping.LocationRequest]@{ Country = "US"; Limit = 1 },
+    [Globalping.LocationRequest]@{ Country = "UK"; Limit = 1 },
+    [Globalping.LocationRequest]@{ Country = "PL"; Limit = 1 },
+    [Globalping.LocationRequest]@{ Country = "FR"; Limit = 1 },
+    [Globalping.LocationRequest]@{ Country = "IT"; Limit = 1 },
+    [Globalping.LocationRequest]@{ Country = "ES"; Limit = 1 },
+    [Globalping.LocationRequest]@{ Country = "NL"; Limit = 1 },
+    [Globalping.LocationRequest]@{ Country = "SE"; Limit = 1 },
+    [Globalping.LocationRequest]@{ Country = "CH"; Limit = 1 }
+)
+Start-Globalping -Type Ping -Target "evotec.xyz" -Verbose -Locations $loc | Format-Table
+
