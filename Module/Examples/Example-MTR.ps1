88094ddacd3a707361fca5fd83bd2303d50c0f2c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-14 20:51:38 +0200

    fix(examples): 🐛 correct variable casing for MTR hops
    * Updated variable `$hops` to `$Hops` for consistency.
    * Ensured proper formatting of MTR hop results.

--- Module/Examples/Example-MTR.ps1
+++ Module/Examples/Example-MTR.ps1
@@ -4,13 +4,14 @@ Start-GlobalpingMtr -Target "evotec.xyz" -Verbose | Format-Table
 
 Start-GlobalpingMtr -Target "evotec.xyz" -Verbose -Classic | Format-Table
 
-$OutputMtr = Start-GlobalpingMtr -Target "evotec.xyz" -Verbose -Raw -SimpleLocations "Krakow+PL","Warsaw+PL", "Berlin+DE", "London+UK", "NewYork+US", "SanFrancisco+US", "Tokyo+JP"
+$OutputMtr = Start-GlobalpingMtr -Target "evotec.xyz" -Verbose -Raw -SimpleLocations "Krakow+PL", "Warsaw+PL", "Berlin+DE", "London+UK", "NewYork+US", "SanFrancisco+US", "Tokyo+JP"
 $OutputMtr | Format-Table
 $OutputMtr.Results | Format-Table
 $OutputMtr.Results.Probe | Format-Table
 $OutputMtr.Results.Data | Format-List
 $OutputMtr.Results[0].Data.RawOutput
-$hops = $OutputMtr.Results[0].ToMtrHops()
-$hops[0] | Format-Table *
-$hops[1] | Format-Table *
+$Hops = $OutputMtr.Results[0].ToMtrHops()
+$Hops | Format-Table *
+$Hops[0] | Format-Table *
+$Hops[1] | Format-Table *
 
